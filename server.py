b6409c142121fa92197b6d3bed736b82d3a08529
Adam Ford <adamjford@gmail.com>
2017-01-22 16:57:16 -0700

    All tests pass

--- server.py
+++ server.py
@@ -35,56 +35,54 @@ class MyWebServer(SocketServer.BaseRequestHandler):
 
         file_path = './www' + path
 
-        if self.content_dir not in os.path.abspath(file_path):
+        # From https://docs.python.org/2/library/os.path.html
+        if not os.path.exists(file_path) \
+                or self.content_dir not in os.path.abspath(file_path):
             # 404
-            pass
+            return self.not_found()
         else:
-            response = ''
-
             # Written by rslite (http://stackoverflow.com/users/15682/rslite) on StackOverflow
             # http://stackoverflow.com/a/82852 (CC-BY-SA 3.0)
             if os.path.isfile(file_path):
                 # There's a file at that path, so let's serve it
-
-                # Source: https://docs.python.org/2/tutorial/inputoutput.html
-                with open(file_path) as f:
-                    contents = f.read()
-
-                headers = [
-                    # Source: Example GET from class slides
-                    # https://eclass.srv.ualberta.ca/pluginfile.php/3259365/mod_resource/content/2/04-HTTP.pdf
-                    'HTTP/1.1 200 OK',
-                    'Server: MyWebServer/0.1 Python/2.7',
-                    # From https://docs.python.org/2/library/mimetypes.html
-                    'Content-type: %s' % mimetypes.guess_type(file_path)[0],
-                    # From https://docs.python.org/2/library/os.path.html#os.path.getsize
-                    'Content-Length: %d' % os.path.getsize(file_path),
-                    'Date: %s' % format_date_now(),
-                    # From https://docs.python.org/2/library/os.path.html#os.path.getmtime
-                    'Last-Modified: %s' % format_date(time.gmtime(os.path.getmtime(file_path)))
-                ]
-
-                response = '\r\n'.join(headers) + '\r\n\r\n'
-                response += contents
+                return self.ok(file_path)
+            # From https://docs.python.org/2/library/os.path.html
             elif os.path.isdir(file_path):
-                # Means we ended up here by /foo
+                # Means we ended up here by /foo (or it would have gotten rewritten to /index.html)
                 # so redirect to /foo/ via 301 Moved Permanently
                 # Based on behaviour/output of SimpleHTTPServer in same case
+                return self.moved_permanently(path)
 
-                now = format_date_now()
+    def moved_permanently(self, path):
+        headers = [
+            'HTTP/1.1 301 Moved Permanently',
+            'Server: MyWebServer/0.1 Python/2.7',
+            'Date: %s' % format_date_now(),
+            'Location: %s' % (path + '/')
+        ]
 
-                headers = [
-                    'HTTP/1.1 301 Moved Permanently',
-                    'Server: MyWebServer/0.1 Python/2.7',
-                    'Date: %s' % now,
-                    'Location: %s' % (path + '/')
-                ]
+        return '\r\n'.join(headers)
 
-                response = '\r\n'.join(headers)
+    def ok(self, file_path):
+        # Source: https://docs.python.org/2/tutorial/inputoutput.html
+        with open(file_path) as f:
+            contents = f.read()
 
-                print response
+        headers = [
+            # Source: Example GET from class slides
+            # https://eclass.srv.ualberta.ca/pluginfile.php/3259365/mod_resource/content/2/04-HTTP.pdf
+            'HTTP/1.1 200 OK',
+            'Server: MyWebServer/0.1 Python/2.7',
+            # From https://docs.python.org/2/library/mimetypes.html
+            'Content-type: %s' % mimetypes.guess_type(file_path)[0],
+            # From https://docs.python.org/2/library/os.path.html#os.path.getsize
+            'Content-Length: %d' % os.path.getsize(file_path),
+            'Date: %s' % format_date_now(),
+            # From https://docs.python.org/2/library/os.path.html#os.path.getmtime
+            'Last-Modified: %s' % format_date(time.gmtime(os.path.getmtime(file_path)))
+        ]
 
-            return response
+        return '\r\n'.join(headers) + '\r\n\r\n' + contents
 
     def handle(self):
         self.content_dir = os.path.abspath('./www/')
@@ -101,7 +99,16 @@ class MyWebServer(SocketServer.BaseRequestHandler):
             response = self.get(split_line[1])
             self.request.sendall(response)
         else:
-            self.request.sendall("501 Not Implemented\r\n")
+            self.request.sendall("HTTP/1.1 501 Not Implemented\r\n")
+
+    def not_found(self):
+        headers = [
+            'HTTP/1.1 404 Not Found',
+            'Server: MyWebServer/0.1 Python/2.7',
+            'Date: %s' % format_date_now()
+        ]
+
+        return '\r\n'.join(headers)
 
 
 def format_date(time_to_format):
