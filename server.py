7a5ea57f6a280fa2a0e381747be54539ce6f741d
Adam Ford <adamjford@gmail.com>
2017-01-22 12:39:56 -0700

    Check for relative paths getting out of current dir

--- server.py
+++ server.py
@@ -35,53 +35,58 @@ class MyWebServer(SocketServer.BaseRequestHandler):
 
         file_path = './www' + path
 
-        response = ''
+        if self.content_dir not in os.path.abspath(file_path):
+            # 404
+            pass
+        else:
+            response = ''
 
-        # Written by rslite (http://stackoverflow.com/users/15682/rslite) on StackOverflow
-        # http://stackoverflow.com/a/82852 (CC-BY-SA 3.0)
-        if os.path.isfile(file_path):
-            # There's a file at that path, so let's serve it
+            # Written by rslite (http://stackoverflow.com/users/15682/rslite) on StackOverflow
+            # http://stackoverflow.com/a/82852 (CC-BY-SA 3.0)
+            if os.path.isfile(file_path):
+                # There's a file at that path, so let's serve it
 
-            # Source: https://docs.python.org/2/tutorial/inputoutput.html
-            with open(file_path) as f:
-                contents = f.read()
+                # Source: https://docs.python.org/2/tutorial/inputoutput.html
+                with open(file_path) as f:
+                    contents = f.read()
 
-            headers = [
-                # Source: Example GET from class slides
-                # https://eclass.srv.ualberta.ca/pluginfile.php/3259365/mod_resource/content/2/04-HTTP.pdf
-                'HTTP/1.1 200 OK',
-                'Server: MyWebServer/0.1 Python/2.7',
-                'Content-type: text/html',
-                # From https://docs.python.org/2/library/os.path.html#os.path.getsize
-                'Content-Length: %d' % os.path.getsize(file_path),
-                'Date: %s' % format_date_now(),
-                # From https://docs.python.org/2/library/os.path.html#os.path.getmtime
-                'Last-Modified: %s' % format_date(time.gmtime(os.path.getmtime(file_path)))
-            ]
+                headers = [
+                    # Source: Example GET from class slides
+                    # https://eclass.srv.ualberta.ca/pluginfile.php/3259365/mod_resource/content/2/04-HTTP.pdf
+                    'HTTP/1.1 200 OK',
+                    'Server: MyWebServer/0.1 Python/2.7',
+                    'Content-type: text/html',
+                    # From https://docs.python.org/2/library/os.path.html#os.path.getsize
+                    'Content-Length: %d' % os.path.getsize(file_path),
+                    'Date: %s' % format_date_now(),
+                    # From https://docs.python.org/2/library/os.path.html#os.path.getmtime
+                    'Last-Modified: %s' % format_date(time.gmtime(os.path.getmtime(file_path)))
+                ]
 
-            response = '\r\n'.join(headers) + '\r\n\r\n'
-            response += contents
-        elif os.path.isdir(file_path):
-            # Means we ended up here by /foo
-            # so redirect to /foo/ via 301 Moved Permanently
-            # Based on behaviour/output of SimpleHTTPServer in same case
+                response = '\r\n'.join(headers) + '\r\n\r\n'
+                response += contents
+            elif os.path.isdir(file_path):
+                # Means we ended up here by /foo
+                # so redirect to /foo/ via 301 Moved Permanently
+                # Based on behaviour/output of SimpleHTTPServer in same case
 
-            now = format_date_now()
+                now = format_date_now()
 
-            headers = [
-                'HTTP/1.1 301 Moved Permanently',
-                'Server: MyWebServer/0.1 Python/2.7',
-                'Date: %s' % now,
-                'Location: %s' % (path + '/')
-            ]
+                headers = [
+                    'HTTP/1.1 301 Moved Permanently',
+                    'Server: MyWebServer/0.1 Python/2.7',
+                    'Date: %s' % now,
+                    'Location: %s' % (path + '/')
+                ]
 
-            response = '\r\n'.join(headers)
+                response = '\r\n'.join(headers)
 
-            print response
+                print response
 
-        return response
+            return response
 
     def handle(self):
+        self.content_dir = os.path.abspath('./www/')
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: \n%s\n" % self.data)
 
